0cee3d54a838a238944609963dd1f76c6220e8d5
Luciana Altamirano. <113325424+LucianaAG@users.noreply.github.com>
2026-07-21 03:54:09 -0300

    Revert "feat: definicion del mock para endpoint /predecir"
    This reverts commit 1fdc98927ec282838f7c7aac542aca31dbf5ba7b.

--- microservicio/app/services/predictor.py
+++
@@ -1,8 +0,0 @@
-from app.schemas import ContenidoEntrada, ContenidoSalida
-
-def predecir_categoria(contenido: ContenidoEntrada) -> ContenidoSalida:
-    return ContenidoSalida(
-        categoria="Backend",
-        probabilidad=0.89,
-        informaciones_adicionales=["Java", "Spring Boot", "API REST"]
-    )
