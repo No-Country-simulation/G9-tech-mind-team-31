70c41a9179f72992239ead8b63ea65c82fa4b472
natimoon <godoy.natali@gmail.com>
2026-08-03 14:36:48 -0300

    fix: add @NotBlank and @Size validation constraints to title field in ContenidoRequestDto

--- api/src/main/java/com/techmind/api/dto/ContenidoRequestDto.java
+++ api/src/main/java/com/techmind/api/dto/ContenidoRequestDto.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ContenidoRequestDto(
+         @NotBlank(message = "El título no puede estar vacío")
+        @Size(min = 3, message = "El título debe contener al menos 3 caracteres")
         String titulo,
         
         @NotBlank(message = "El texto no puede estar vacío")
         @Size(min = 10, message = "El texto debe contener al menos 10 caracteres")
         String texto
-) {}
+) {}    
