0ac3c151708153e46e6f83d2499b8bb03b88b6b9
Zuggeits Meriño <160367140+Zumeram@users.noreply.github.com>
2026-08-14 16:35:38 -0400

    Delete servicios/src/SERVICE/ClasificacionService.java

--- servicios/src/SERVICE/ClasificacionService.java
+++
@@ -1,9 +0,0 @@
-package com.techmind.api.service;
-
-import com.techmind.api.dto.ContenidoRequest;
-import com.techmind.api.dto.ContenidoRespuesta;
-
-public interface ClasificacionService {
-    ContenidoRespuesta clasificar(ContenidoRequest request);
-    boolean estaDisponible();
-}
